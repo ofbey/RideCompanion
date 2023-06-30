7d5d97f2ceedd7be5a142c86491c7edd42716e0c
ofbey <beyhatunfatih@gmail.com>
2023-06-29 22:39:57 -0700

    CSRF updated

--- ridecompanion/settings.py
+++ ridecompanion/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.environ.get('DEBUG').lower() == 'true'
 
 ALLOWED_HOSTS = ['https://ridecompanion-production.up.railway.app','127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://ridecompanion-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://ridecompanion-production.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
